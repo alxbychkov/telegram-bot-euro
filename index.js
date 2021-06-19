8ee566e1c53aa1b2098dd2b5b976f45d530cf912
Alexander Bychkov <alx.bychkov@gmail.com>
2021-06-19 12:54:43 +0300

    add data

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bot = new telegramApi(token, botOptions)
 bot.setMyCommands(botCommands)
 
 let chats = {}
-let chatGames = {}
+let chatsGames = {}
 
 const init = async () => {
     try {
@@ -20,8 +20,14 @@ const init = async () => {
     }
 
     await ChatModel.findAll().then(res => {
-        if (res) res.map(i => chats[i.get("chatId")] = {})
+        if (res) res.map(async (i) => {
+            const chatId = i.get("chatId")
+            const result = await FriendModel.findOne({chatId,where:{}})
+            result.result ? chats[chatId] = JSON.parse(result.get("result")) : chats[chatId] = {}
+        })
     })
+
+
 
     bot.on('message', async msg => {
         const chatId = msg.chat.id
@@ -44,15 +50,15 @@ const init = async () => {
             if (text === '/stats') {
                 const user = await FriendModel.findOne({chatId,name, where: {}})
                 const userRes = JSON.parse(user.result)
-                console.log(userRes[0].id) 
                 await bot.sendMessage(chatId, `Тут скоро будет статистика. Потерпи, ${user.name}.`)
             }
-            if (text === '1-1' && chatsGames[chatId]) {
+            if (text[0] === '%' && chatsGames[chatId]) {
                 const user = await FriendModel.findOne({chatId,name, where: {}})
-                // chats[chatId] = JSON.stringify([{id:gameId,match:text}])
-                chats[chatId][chatsGames[chatId]] = text
-                // user.result = result
-                // await user.save()
+                chats[chatId][chatsGames[chatId]] = text.split('%')[1]
+
+                user.result = JSON.stringify(chats[chatId])
+                await user.save()
+                await bot.sendMessage(chatId, `Ставка принята!`)
             }
             console.log(chats)
         } catch (e) {
@@ -74,24 +80,14 @@ const init = async () => {
             const game = todayGames.find(el => el.id === +data).game
             const id = todayGames.find(el => el.id === +data).id
 
+            if (chats[chatId][id] !== '' && chats[chatId][id] !== undefined) return bot.sendMessage(chatId, `Ты уже поставил на этот матч ${chats[chatId][id]}. Отдыхай =)`)
+
             if (chats[chatId]) {
                 chatsGames[chatId] = id
                 chats[chatId][id] = ''
             }
 
-            // if (chats[chatId]) 
-            //     chats[chatId].push({
-            //         id: todayGames.find(el => el.id === +data).id,
-            //         match: ''
-            //     })
-            // else {
-            //     chats[chatId] = [{
-            //         id: todayGames.find(el => el.id === +data).id,
-            //         match: ''
-            //     }]
-            // }
-
-            await bot.sendMessage(chatId, `Ты выбрал матч ${game}. Напиши как он по твоему закончиться?`)
+            await bot.sendMessage(chatId, `Ты выбрал матч ${game}. Напиши как он по твоему закончиться? (Напиши счет начиная с %. пример: %1-1)`)
         }
     })
 }
@@ -100,7 +96,10 @@ init()
 
 async function showMatches(chatId) {
     const now = new Date()
-    const games = 3
+    let games = 3
+    
+    if (todayGames.filter(el => el.date === now.toLocaleDateString()))
+        games = todayGames.filter(el => el.date === now.toLocaleDateString()).length
     const match = (games === 1) ? 'матч' : 'матча'
     const gameOptions = {reply_markup: ''}
 
@@ -110,7 +109,7 @@ async function showMatches(chatId) {
     if (games > 0) {
         let dayGames = {inline_keyboard: []}
         for(let i=0; i<games; i++) {
-            dayGames.inline_keyboard.push([{text: todayGames[i].game, callback_data: todayGames[i].id}])
+            dayGames.inline_keyboard.push([{text: todayGames.filter(el => el.date === now.toLocaleDateString())[i].game, callback_data: todayGames.filter(el => el.date === now.toLocaleDateString())[i].id}])
         }
         gameOptions.reply_markup = JSON.stringify(dayGames)
         await bot.sendMessage(chatId, `Сегодня ${now.toLocaleDateString()}. И в программне ${games} ${match}. На кого будешь ставить?`, gameOptions)
